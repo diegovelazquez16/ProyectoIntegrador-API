2aef0cd25315f3ef311eb6dd8e5aabf8595b3b6b
Diego Velazquez <233304@ids.upchiapas.edu.mx>
2024-07-08 08:38:19 -0600

    CRUDs_ funcionales

--- src/controllers/ventas.js
+++ src/controllers/ventas.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "mysql",
-  database: process.env.DB_NAME || "tiendaUniformesDeportivos3"
+  host: "localhost",
+  user:  "root",
+  password:  "mysql",
+  database:"tiendaUniformesDeportivos3"
 });
 
 db.connect((err) => {
