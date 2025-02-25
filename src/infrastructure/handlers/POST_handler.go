98d6812f36ec1616502b99b7063c0cea75aa5dd0
Diego Velazquez <govelazmen@gmail.com>
2025-02-24 18:07:47 -0600

    Add: t4

--- src/infrastructure/handlers/POST_handler.go
+++ src/infrastructure/handlers/POST_handler.go
@@ -8,7 +8,7 @@ import (
 )
 ///pull
 // prueba pull #2
-// priueba pull #3
+// priueba pull #4
 func GithubWebhookHanlder(ctx *gin.Context) {
 	eventType := ctx.GetHeader("X-GitHub-Event")
 	deliveryID := ctx.GetHeader("X-GitHub-Delivery")
